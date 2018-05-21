05b190cdf7f910d86b7349de7387a51220d0f32f
michaelcraig <m_craig@baylor.edu>
2018-05-21 17:39:48 -0400

    start button displays prompt

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 
-let openPrompt = prompt('WHO CHALLENGES SIMON?', "You can't win")
+let userScore = {
+    user: "",
+    score: 0,
 
-// openPrompt()
+    startButton: function () {
+        let pName = prompt('WHO CHALLENGES SIMON?', "You can't win")
+        if (pName != null) {
+            this.user = pName
+        }
+        console.log(userScore)
 
+    }
+}
+
+$(() => {
+    $('.startBtn').click(userScore.startButton)
+})
