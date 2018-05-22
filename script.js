f7dbaea3474e126ad1929ae173c97d0920b92a36
michaelcraig <m_craig@baylor.edu>
2018-05-22 12:43:27 -0400

    color change with simon value functionality added

--- script.js
+++ script.js
@@ -9,43 +9,58 @@ let userScore = {
 
 $(() => {
 
-showSimonMove = function () {
-    
-}
-
-startGame = function () {
-    // reset()?
-    let randomBtnValue = userScore.btnValues[Math.floor(Math.random() * userScore.btnValues.length)]
-    userScore.simonValues.push(randomBtnValue)
-    showSimonMove()
-}
-
-startButton = function () {
-    let pName = prompt('WHO CHALLENGES SIMON?', "You cannot win")
-    if (pName != null) {
-        userScore.user = pName
-        startGame()
+    showSimonMove = function () {
+        let simonArr = userScore.simonValues
+        for (i = 0; i < simonArr.length; i++) {
+            if (simonArr[i] === '.b1') {
+                $('.b1 a').removeClass('blue')
+                $('.b1 a').addClass('black')
+            } else if (simonArr[i] === '.r2') {
+                $('.r2 a').removeClass('red')
+                $('.r2 a').addClass('black')
+            } else if (simonArr[i] === '.g3') {
+                $('.g3 a').removeClass('green')
+                $('.g3 a').addClass('black')
+            } else if (simonArr[i] === '.y4') {
+                $('.y4 a').removeClass('yellow')
+                $('.y4 a').addClass('black')
+            }
+        }
     }
-}
+
+    startGame = function () {
+        // reset()?
+        let randomBtnValue = userScore.btnValues[Math.floor(Math.random() * userScore.btnValues.length)]
+        userScore.simonValues.push(randomBtnValue)
+        showSimonMove()
+    }
+
+    startButton = function () {
+        let pName = prompt('WHO CHALLENGES SIMON?', "You cannot win")
+        if (pName != null) {
+            userScore.user = pName
+            startGame()
+        }
+    }
 
 
-if (uservalue != simonValue)
-    gameOver()
+    // if (uservalue != simonValue)
+    //     gameOver()
 
-bleh = function () {
-    console.log(userScore)
-}
+    bleh = function () {
+        console.log(userScore)
+    }
 
-gameOver = function () {
-    let finalScore = userValues.length
-    alert(finalScore)
-}
+    gameOver = function () {
+        let finalScore = userValues.length
+        alert(finalScore)
+    }
 
-reset = function () {
-    userScore.score = 0
-    userScore.simonValues = []
-    userScore.userValues = []
-}
+    reset = function () {
+        userScore.score = 0
+        userScore.simonValues = []
+        userScore.userValues = []
+    }
 
     $('.startBtn').click(startButton)
     $('.b1').click(bleh)
