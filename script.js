f86fb09fb61364ab591f85f26be9d56e72a9d0e2
michaelcraig <m_craig@baylor.edu>
2018-05-22 12:08:39 -0400

    created gameOver and reset functions

--- script.js
+++ script.js
@@ -9,10 +9,15 @@ let userScore = {
 
 $(() => {
 
+showSimonMove = function () {
+    
+}
+
 startGame = function () {
     // reset()?
     let randomBtnValue = userScore.btnValues[Math.floor(Math.random() * userScore.btnValues.length)]
     userScore.simonValues.push(randomBtnValue)
+    showSimonMove()
 }
 
 startButton = function () {
@@ -23,13 +28,28 @@ startButton = function () {
     }
 }
 
+
+if (uservalue != simonValue)
+    gameOver()
+
 bleh = function () {
     console.log(userScore)
 }
 
+gameOver = function () {
+    let finalScore = userValues.length
+    alert(finalScore)
+}
+
+reset = function () {
+    userScore.score = 0
+    userScore.simonValues = []
+    userScore.userValues = []
+}
+
     $('.startBtn').click(startButton)
     $('.b1').click(bleh)
-    $('.r2').click(startGame)
+    // $('.r2').click(startGame)
     // $('.g3').click(userScore)
     // $('.y4').click(userScore)
 })
