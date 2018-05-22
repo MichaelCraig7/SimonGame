481ca4235bfb11287d4b1bfe418418cfbd10d78c
michaelcraig <m_craig@baylor.edu>
2018-05-22 11:38:53 -0400

    misc errors fixed, username add works

--- script.js
+++ script.js
@@ -5,27 +5,34 @@ let userScore = {
     btnValues: ['.b1', '.r2', '.g3', '.y4'],
     simonValues: [],
     userValues: [],
-
-    startButton: function () {
-        let pName = prompt('WHO CHALLENGES SIMON?', "You cannot win")
-        if (pName != null) {
-            this.user = pName
-        }
-    },
-
-
-    startGame: function () {
-        // reset()?
-        let randomBtnValue = userScore.btnValues[Math.floor(Math.random() * userScore.btnValues.length)]
-        $(randomBtnValue).append(userScore.simonValues)
-        // userScore.simonValues.push(randomBtnValue)
-    },
 }
-// $('b1').append(userValues)
-// $('r2').append(userValues)
-// $('g3').append(userValues)
-// $('y4').append(userValues)
 
 $(() => {
-    $('.startBtn').click(userScore.startGame)
+
+startGame = function () {
+    // reset()?
+    let randomBtnValue = userScore.btnValues[Math.floor(Math.random() * userScore.btnValues.length)]
+    $(randomBtnValue).append(userScore.simonValues)
+}
+
+startButton = function () {
+    let pName = prompt('WHO CHALLENGES SIMON?', "You cannot win")
+    if (pName != null) {
+        userScore.user = pName
+        startGame()
+    }
+}
+
+bleh = function () {
+    console.log(userScore)
+}
+
+})
+
+$(() => {
+    $('.startBtn').click(startButton)
+    $('.b1').click(bleh)
+    $('.r2').click(startGame)
+    // $('.g3').click(userScore)
+    // $('.y4').click(userScore)
 })
