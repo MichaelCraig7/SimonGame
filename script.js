f05b71ab1502b880cafa4bc4776fc6518794698d
michaelcraig <m_craig@baylor.edu>
2018-05-22 10:39:13 -0400

    start game function

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 let userScore = {
     user: '',
     score: 0,
+    btnValues: ['.b1', '.r2', '.g3', '.y4'],
+    simonValues: [],
+    userValues: [],
 
     startButton: function () {
         let pName = prompt('WHO CHALLENGES SIMON?', "You cannot win")
@@ -9,17 +12,20 @@ let userScore = {
             this.user = pName
         }
         console.log(userScore)
-    
+        startGame()
     }
-}
+},
 
-simonValues = []
-userValues = []
+startGame: function () {
+    for (i=0; )
+    // reset()?
+    btnValues[Math.floor(Math.random() * btnValues.length)];
+},
 
-$('b1').append(userValues)
-$('b1').append(userValues)
-$('b1').append(userValues)
-$('b1').append(userValues)
+// $('b1').append(userValues)
+// $('r2').append(userValues)
+// $('g3').append(userValues)
+// $('y4').append(userValues)
 
 $(() => {
     $('.startBtn').click(userScore.startButton)
